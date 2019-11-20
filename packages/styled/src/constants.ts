09dec82b651b6ca4fed1b285a9b8a001725e039d
mg901 <mgreen097@gmail.com>
2019-11-20 23:37:59 +0300

    fix: replace string with array of string in defaultConfig

--- packages/styled/src/constants.ts
+++ packages/styled/src/constants.ts
@@ -2,13 +2,13 @@ export const CONFIG_SYMBOL = '@typographist/styled';
 export const BROWSER_CONTEXT = 16;
 
 export type DefaultConfig = {
-  base: '16px';
+  base: ['16px'];
   lineHeight: 1.5;
   ratio: 1.333;
 };
 
 export const DEFAULT_CONFIG: DefaultConfig = {
-  base: '16px',
+  base: ['16px'],
   lineHeight: 1.5,
   ratio: 1.333,
 };
