53e26774385ebed7a1d8e4789aeff124bb12954f
mg901 <mgreen097@gmail.com>
2019-11-21 17:16:19 +0300

    chore: update postcss example

--- examples/postcss/postcss.config.js
+++ examples/postcss/postcss.config.js
@@ -1,24 +1,24 @@
-const { typographist, ratios } = require('@typographist/postcss');
+const { typographist, ratios } = require('../../packages/postcss/src');
 
 module.exports = {
   plugins: [
     typographist({
-      base: '16px',
+      base: ['16px'],
       lineHeight: 1.5,
       ratio: ratios.MINOR_SECOND,
       tablet: {
         breakpoint: '768px',
-        base: '17px',
+        base: ['17px'],
         ratio: ratios.MAJOR_SECOND,
       },
       desktop: {
         breakpoint: '992px',
-        base: '19px',
+        base: ['19px'],
         ratio: ratios.MINOR_THIRD,
       },
       lgDesktop: {
         breakpoint: '1200px',
-        base: '21px',
+        base: ['21px'],
       },
     }),
   ],
