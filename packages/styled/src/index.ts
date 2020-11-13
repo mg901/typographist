5ba6f0f51374ec33879b05cbd2437f55cf77129e
mg901 <mgreen097@gmail.com>
2020-11-13 20:27:02 +0300

    fix: fix path for makeStyledBreakpoints

--- packages/styled/src/index.ts
+++ packages/styled/src/index.ts
@@ -1,4 +1,4 @@
-import { _makeStyledBreakpoints } from 'styled-breakpoints';
+import { makeStyledBreakpoints } from 'styled-breakpoints/core';
 import { CONFIG_SYMBOL } from './constants';
 
 export { ratios } from '@typographist/core';
@@ -6,7 +6,7 @@ export { step } from './features/step';
 export { steps } from './features/steps';
 export { TypographistProvider } from './provider';
 
-export const { up, down, between, only } = _makeStyledBreakpoints({
+export const { up, down, between, only } = makeStyledBreakpoints({
   errorPrefix: '[typographist]: ',
   pathToMediaQueries: [CONFIG_SYMBOL, 'mediaQueries'],
 });
